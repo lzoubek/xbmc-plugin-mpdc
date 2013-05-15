083e63508349837e78b31a3fca017ae00e2218a2
Libor Zoubek <lzoubek@jezzovo.net>
2013-05-15 23:44:22 +0200

    removed obsolete files

--- default.py
+++ default.py
@@ -31,50 +31,6 @@ import xbmpc
 import mpdutil as util
 
 import mpdcontrol as mpd
-#do http request
-def request(url):
-    req = urllib2.Request(url)
-    response = urllib2.urlopen(req)
-    data = response.read()
-    response.close()
-    return data
-
-def replace(obj,what=None,replacement=''):
-    if obj == what:
-        return replacement
-    return obj
-
-def add_stream(name,url,bitrate,logo):
-    bit = 0
-    try:
-        bit = int(bitrate)
-    except:
-        pass
-    name = replace(name,None,'No name').replace('&amp;','&')
-    logo = replace(logo)
-    url=sys.argv[0]+"?play="+url
-    li=xbmcgui.ListItem(name,path = url,iconImage="DefaultAudio.png",thumbnailImage=logo)
-    li.setInfo( type="Music", infoLabels={ "Title": name,"Size":bit } )
-    li.setProperty("IsPlayable","true")
-    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=li,isFolder=False)
-
-# retrieve target stream if url is asx or m3u
-def parse_playlist(url):
-    if url.endswith('m3u'):
-        return request(url).strip()
-    if not url.endswith('asx'):
-        return url
-    data = request(url)
-    refs = re.compile('.*<Ref href = \"([^\"]+).*').findall(data,re.IGNORECASE|re.DOTALL|re.MULTILINE)
-    urls = []
-    for ref in refs:
-        stream = parse_playlist(ref)
-        urls.append(stream.replace(' ','%20'))
-    if urls == []:
-        print 'Unable to parse '+url
-        print data
-        return ''
-    return urls[-1]
 
 def get_params(url=None):
     if url == None:
@@ -96,58 +52,6 @@ def get_params(url=None):
     for p in param.keys():
         param[p] = param[p].decode('hex')
     return param
-
-def download_stationfile(dest):
-    print 'Saving station file'
-    data = request('http://abradio.cz/external/rss/radia.xml')
-    f = open(dest,'w')
-    f.write(unicode(data,'UTF-8').encode('UTF-8'))
-    f.close()
-
-def get_data():
-    local = xbmc.translatePath(__addon__.getAddonInfo('profile'))
-    if not os.path.exists(local):
-        os.makedirs(local)
-    local = os.path.join(local,'stations.xml')
-    if os.path.exists(local):
-        # update local station file when it becomes 1day old
-        if (time.time() - os.path.getctime(local)) > (3600*24):
-            download_stationfile(local)
-    else:
-        download_stationfile(local)
-    return ET.parse(local)
-
-def list_categories():
-    tree = get_data()
-    categories = {}
-    for category in tree.findall('ABRADIOITEM/CATEGORY'):
-        id = category.get('ID')
-        categories[category.get('ID')]=category.text
-    for id in categories.keys():
-        add_dir(categories[id],'category='+id,'')
-        add_dir(__language__(30000),'category=-1','')
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def list_category(id):
-    tree = get_data()
-    for station in tree.findall('ABRADIOITEM'):
-        category = station.find('CATEGORY')
-        if int(id) < 0 or id == category.get('ID'):
-            add_dir(station.find('RADIO').text,'station='+station.find('ID').text,station.find('LOGO').text)
-    xbmcplugin.addSortMethod( handle=int(sys.argv[1]), sortMethod=xbmcplugin.SORT_METHOD_LABEL, label2Mask="%X")
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
-
-def resolve_station(id):
-    tree = get_data()
-    for station in tree.findall('ABRADIOITEM'):
-        if station.find('ID').text == id:
-            name = station.find('RADIO').text
-            logo = station.find('LOGO').text
-            for stream in station.findall('STREAMS/*'):
-                add_stream(name,stream.text,stream.get('name'),logo)
-                xbmcplugin.addSortMethod( handle=int(sys.argv[1]), sortMethod=xbmcplugin.SORT_METHOD_LABEL, label2Mask="%X")
-                xbmcplugin.addSortMethod( handle=int(sys.argv[1]), sortMethod=xbmcplugin.SORT_METHOD_BITRATE, label2Mask="%X")
-            return xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def get_stream():
     stream_url = __sett__('stream_url')
